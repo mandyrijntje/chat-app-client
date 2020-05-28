5bc4aa6d192b300fbc42e024d4c9e404b892bfab
messmonte <amandamariamonte@gmail.com>
2020-05-28 18:48:57 +0200

    Refactored store.js to delete thunk

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import reducers from "./reducer";
-import ReduxThunk from "redux-thunk";
+import reducer from "./reducer";
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhancer = composeEnhancers(applyMiddleware(ReduxThunk));
-const store = createStore(reducers, enhancer);
+
+const store = createStore(reducer, composeEnhancers());
+
 export default store;
